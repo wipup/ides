400675d0d05efe339449d7edd7aa59ec2b8f9dc9
wipup <wipu_p@hotmail.com>
2019-02-24 02:04:10 +0700

    add interface for OpenGraph OG Tags

---
+++ src/main/java/ports/soc/ides/model/socialmedia/BasicOpenGraphModel.java
@@ -0,0 +1,12 @@
+package ports.soc.ides.model.socialmedia;
+
+public interface BasicOpenGraphModel {
+
+	public String getOgUrl();
+	
+	public String getOgTitle();
+	
+	public String getOgDescription();
+	
+	
+}
